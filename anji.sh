90d8dee4c468469968750dea55e04bcab36923c1
anji <anji532@gmail.com>
2017-10-06 17:22:32 +0530

    Changes

--- anji.sh
+++ anji.sh
@@ -5,30 +5,40 @@ echo "please enter num1"
 read num1
 
 while true; do
-echo  -n "Please enter 2 input"
+echo  "Please enter 2 input"
 read num2 
 	
 	case $num2 in
-		0) echo "Please enter a valid input for division"
-			continue;;
+	
+	0) echo "Please enter a valid input for division"
+			
+	continue;;
 			
 		#*[a-zA-Z]*) echo "please enter a number except zero "
 			
 			    # continue;;
-			 
-		     *[!0-9]*) echo "Please enter a number except zero"
-			     continue;;
+	
+	*[!0-9]*) echo "Please enter a number except zero"
+	
+	continue;;
 		     
-		     *) 
-		sum=`expr $num1 / $num2`
+	 *) 
+		Div=`expr $num1 / $num2`
 	
 		mul=`expr $num1 \\* $num2`
-	
-		echo "multiplcation is $mul"
+
+tput setaf 2
+		echo "******************************"
+		echo "Arithamatic Operations "
 		echo "first number is $num1"
 		echo "secound number is $num2"
-		echo "Division is $sum"
-		     break;;
+		
+		echo "multiplcation is $mul"
+		echo "Division is $Div"
+		echo "******************************"
+	tput sgr0	
+		
+	   break;;
      esac
 
 		     done
