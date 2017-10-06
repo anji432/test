4f28bf6496f44fa66a1eb79a3c0a96bd4bbfd2f4
anji <anji532@gmail.com>
2017-10-06 16:09:08 +0530

    Divsion script

---
+++ anji.sh
@@ -0,0 +1,32 @@
+
+
+#!/bin/bash
+echo "please enter num1"
+read num1
+
+num2=0
+while true; do
+echo  -n "Please enter 2 input"
+read num2 
+	
+	case $num2 in
+		0) echo "Please enter a valid input for division"
+			continue;;
+			
+		#*[a-zA-Z]*) echo "please enter a number except zero "
+			
+			    # continue;;
+			 
+		     *[!0-9]*) echo "Please enter a number except zero"
+			     continue;;
+		     
+		     *) 
+		sum=`expr $num1 / $num2`
+		echo "first number is $num1"
+		echo "secound number is $num2"
+		echo "Division is $sum"
+		     break;;
+     esac
+
+		     done
+
